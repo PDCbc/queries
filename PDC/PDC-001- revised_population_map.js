43f19597243631df66f21339cc55bb62cfd95725
Derek Roberts <derek.roberts@gmail.com>
2015-02-20 20:06:39 -0800

    PDC-001 edge cases and typos

--- PDC/PDC-001- revised_population_map.js
+++ PDC/PDC-001- revised_population_map.js
@@ -2,13 +2,13 @@
 // Query Title: Population profile by age and gender
 
 function map( patient ){
-  // Store physicial ID, use a JSON key
+  // Store physician ID, via JSON key
   var pid = "_" + patient.json.primary_care_provider_id;
 
-  //Store age and gender, use patient Object functions
+  //Store age and gender, via patient Object functions
   var age = patient.age();
   var gdr = patient.gender();
-  
+
   // Convert gdr to an expected value
   if( gdr === null || gdr === undefined ){
     gdr = "undifferentiated_";
@@ -22,29 +22,27 @@ function map( patient ){
     else
       gdr = "undifferentiated_";
   }
-  
-  // Identify range and emit accordingly
-  // Note: all ranges are emitted, including 0s
-  if( typeof age === 'undefined' || typeof age === 'null' ){
-    // Special case for undefined/null
-    emit("age_unspecified" + pid, 1);
+
+  // Edge cases assigned -1 (out of specified ranges)
+  if( typeof age !== 'number' || age < 0 || age > 120 ){
+    emit( "age_unspecified" + pid, 1 );
+    age = -1;
   }
-  else { 
-    // Special case for 90+
-    if( age >= 90 )
-      emit( gdr + "_90+" + pid, 1 );
+
+  // Emit for 90+ special case
+  if( age >= 90 )
+    emit( gdr + "_90+" + pid, 1 );
+  else
+    emit( gdr + "_90+" + pid, 0 );
+
+  // Emit for remaining ranges (of 10 yrs)
+  var i = 80;
+  for( ; i >= 0; i-=10 ){
+    var range = i + "-" +( i + 9 );
+    if( age >= i && age <( i + 10 ))
+      emit( gdr + range + pid, 1 );
     else
-      emit( gdr + "_90+" + pid, 0 );
-    
-    // Iterate through remaining ranges (of 10 yrs)
-    var i = 80;
-    for( ; i >= 0; i-=10 ){
-      var range = i + "-" +( i + 9 );
-      if( age >= i && age <( i + 10 ))
-        emit( gdr + range + pid, 1 );
-      else
-        emit( gdr + range + pid, 0 );
-    }
+      emit( gdr + range + pid, 0 );
   }
 
   // Denominator is counted every time
