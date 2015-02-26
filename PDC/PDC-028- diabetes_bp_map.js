5d299e2ff5b97c36d4107a71ec7bd083050eacd3
Derek Roberts <DerekRoberts@users.noreply.github.com>
2015-02-26 15:13:33 -0800

    PDC-028 Revised: shorter, +performance, +comments

--- PDC/PDC-028- diabetes_bp_map.js
+++ PDC/PDC-028- diabetes_bp_map.js
@@ -1,107 +1,94 @@
-// Reference Number: PDC-028
-// Query Title: Diabetes & BP <= 130/80 in last yr
+/**
+* Query Title: PDC-028
+* Description: Diabetics with all BP <= 130/80 in last yr
+*
+* Explanation: Of patients with diabetes, how many have had their blood pressure
+*              recorded in the last year, such that BP <= 130/80?
+*/
 
-function map(patient) {
+function map( patient ){
+  // Store physician ID, via JSON key
+  var pid = "_" + patient.json.primary_care_provider_id;
 
-    var targetBloodPressureSystolicCodes = {
-        "LOINC": ["8480-6"]
-    };
 
-    var targetBloodPressureDiastolicCodes = {
-        "LOINC": ["8462-4"]
-    };
+  /**
+  * Denominator
+  *
+  * Values:
+  *  no restriction
+  *
+  * Sets:
+  *   list of conditions, code for diabetes, code filtered match
+  */
+  var denomiList    = patient.conditions(),
+      denomiCodes   ={ "ICD9" :[ "250*" ]},
+      denomiMatches = denomiList.regex_match( denomiCodes );
 
-    var targetProblemCodes = {
-        "ICD9": ["250*"]
-    };
 
-    var bpSystolicLimit = 130;
-    var bpDiastolicLimit = 80;
+  /**
+  * Target Group
+  *
+  * Values:
+  *   1 year ago <= date <= now, 0 <= LDL <= 2.5 (=tMax)
+  *
+  * Sets:
+  *   list of test results, code for systolic, code for diastolic,
+  *   date filtered matches for systolic, date filtered matches for diastolic
+  */
+  var targetEnd   = new Date(),
+      targetStart = new Date( targetEnd.getFullYear() - 1, targetEnd.getMonth(), targetEnd.getDate() ),
+      targetMin_S = 0,
+      targetMax_S = 130.0,
+      targetMin_D = 0,
+      targetMax_D = 80.0;
 
-    var problemList = patient.conditions();
-    var vitalSignList = patient.vitalSigns();
+  var targetList      = patient.vitalSigns(),
+      targetCodes_S   ={ "LOINC":[ "8480-6" ]},
+      targetCodes_D   ={ "LOINC":[ "8462-4" ]},
+      targetMatches_S = targetList.match( targetCodes_S, targetStart, targetEnd ),
+      targetMatches_D = targetList.match( targetCodes_D, targetStart, targetEnd );
 
-    var now = new Date(2013, 10, 30);
-    var start = addDate(now, -1, 0, 0);  // last 12 months
-    var end = addDate(now, 0, 0, 0);
 
-    // Shifts date by year, month, and date specified
-    function addDate(date, y, m, d) {
-        var n = new Date(date);
-        n.setFullYear(date.getFullYear() + (y || 0));
-        n.setMonth(date.getMonth() + (m || 0));
-        n.setDate(date.getDate() + (d || 0));
-        return n;
-    }
+  /**
+  * Emit Numerator and Denominator
+  *
+  * Denominator:  Diabetics
+  * Target Group: LDL <= 130/80, recorded in last year
+  * Numerator:    Intersection of denominator and target
+  */
+  var denominator = checkDenominator( denomiMatches ),
+      numerator   = denominator && checkTarget( targetMatches_S, targetMin_S, targetMax_S)
+                    && checkTarget( targetMatches_D, targetMin_D, targetMax_S );
+      emit( "denominator" + pid, denominator );
+      emit( "numerator"   + pid, numerator   );
+}
 
-    // Checks if patient is older than ageLimit
-    function population() {
+
+/**
+* Checks the patient for a denominator match
+*
+*   1 or 0: diabetic?
+*/
+function checkDenominator( matches ) {
+  return 0 < matches.length;
+}
+
+
+/**
+* Checks the patient for a target group match
+*
+*   1 or 0: BP <= 130/80? (already filtered by date)?
+*     0 <= systolic <= 130, 0 <= diastolic <= 80
+*/
+function checkTarget( matches, min, max ) {
+
+  if(! matches.length )
+    return false;
+
+  for( var i = 0, l = matches.length; i < l; i++ ){
+    var value = matches[ i ].values()[ 0 ].scalar();
+      if( min <= value && value <= max )
         return true;
-    }
-
-    // Checks for existence of systolic blood pressure observation
-    function hasSystolicBloodPressure() {
-        return vitalSignList.match(targetBloodPressureSystolicCodes, start, end).length;
-    }
-
-    // Checks for existence of diastolic blood pressure observation
-    function hasDiastolicBloodPressure() {
-        return vitalSignList.match(targetBloodPressureDiastolicCodes, start, end).length;
-    }
-
-    function hasBloodPressure() {
-        return hasSystolicBloodPressure() && hasDiastolicBloodPressure();
-    }
-
-    // Checks for diabetic patients
-    function hasProblemCode() {
-        return problemList.regex_match(targetProblemCodes).length;
-    }
-
-    // Checks for Blood Pressure value matching conditions
-    // Systolic and diastolic records do not need to be sequential to return true
-    function hasBloodPressureMatchingIndicators() {
-        var bpSystolic = Number.MAX_VALUE;
-        var bpDiastolic = Number.MAX_VALUE;
-        for (var i = 0; i < vitalSignList.length; i++) {
-            if (vitalSignList[i].includesCodeFrom(targetBloodPressureSystolicCodes) &&
-                vitalSignList[i].timeStamp() > start) {
-                if (vitalSignList[i].values()[0].units() !== null &&
-                    vitalSignList[i].values()[0].units().toLowerCase() === "mm[Hg]".toLowerCase()) {
-                    if(vitalSignList[i].values()[0].scalar() < bpSystolic) {
-                        bpSystolic = vitalSignList[i].values()[0].scalar();
-                    }
-                }
-            } else if (vitalSignList[i].includesCodeFrom(targetBloodPressureDiastolicCodes) &&
-                       vitalSignList[i].timeStamp() > start) {
-                if (vitalSignList[i].values()[0].units() !== null &&
-                    vitalSignList[i].values()[0].units().toLowerCase() === "mm[Hg]".toLowerCase()) {
-                    if(vitalSignList[i].values()[0].scalar() < bpDiastolic) {
-                        bpDiastolic = vitalSignList[i].values()[0].scalar();
-                    }
-                }
-            }
-
-            if (bpSystolic > 0 && bpDiastolic > 0 &&
-                bpSystolic <= bpSystolicLimit && bpDiastolic <= bpDiastolicLimit) {
-                return true
-            }
-        }
-        return false;
-    }
-
-    emit('total_pop', 1);
-
-    if (population()) {
-        if (hasProblemCode()) {
-            emit("denominator_diabetics", 1);
-            if (hasBloodPressure() && hasBloodPressureMatchingIndicators()) {
-                emit("numerator_diabetics_bp", 1);
-            }
-        }
-    }
-
-    // Empty Case
-    emit("numerator_diabetics_bp", 0);
-    emit("denominator_diabetics", 0);
+  }
+  return false;
 }
