0505db9df5aeaa756ae5b577bd84a17b39f63653
Derek Roberts <DerekRoberts@users.noreply.github.com>
2015-02-26 00:06:49 -0800

    PDC-027 Revised: shorter, +performance, +comments

--- PDC/PDC-027- diabetes_ldl_map.js
+++ PDC/PDC-027- diabetes_ldl_map.js
@@ -1,73 +1,51 @@
 // Reference Number: PDC-027
-// Query Title: Diabetics with LDL in last yr <= 2.5
-// TODO: Add freetext definition search
+// Query Title: Diabetics with LDL in last yr, LDL <= 2.5
+//
+// Denominator: diabetics
+// Target: LDL <= 2.5, recorded in last year
+// Numerator: intersection of denominator and target
+//
+// Explanation: Of patients with diabetes, how many have had LDL recorded in
+//   the last year, such that LDL <= 2.5?
+//
+function map( patient ){
+  // Store physician ID, via JSON key
+  var pid = "_" + patient.json.primary_care_provider_id;
 
-function map(patient) {
-    var targetLabCodes = {
-        "pCLOCD": ["39469-2"]
-    };
+  // Denominator: list of conditions, code indicating diabetes, matches
+  var dConditions   = patient.conditions(),
+      dCondiCodes   ={ "ICD9" :[ "250*" ]},
+      dCondiMatches = dConditions.regex_match( dCondiCodes );
 
-    var targetProblemCodes = {
-        "ICD9": ["250*"]
-    };
+  // Target dates: ends now, starts one year ago
+  var end   = new Date(),
+      start = new Date( end.getFullYear() - 1, end.getMonth(), end.getDate() );
 
-    var ldlLimit = 2.5;
+  // Target: list of test results, code for HGBA1C, matches in date range
+  var dTResults  = patient.results(),
+      dTResCodes ={ "pCLOCD" :[ "39469-2" ]},
+      dTRMatches = dTResults.match( dTResCodes, start, end );
 
-    var resultList = patient.results();
-    var problemList = patient.conditions();
+  // Target: LDL <= 2.5
+  var tgtMax = 2.5;
 
-    var now = new Date(2013, 10, 30);
-    var start = addDate(now, -1, 0, 0);
-    var end = addDate(now, 0, 0, 0);
+  // 1 or 0: diabetic?
+  function checkDenominator() {
+    return 0 < dCondiMatches.length;
+  }
 
-    // Shifts date by year, month, and date specified
-    function addDate(date, y, m, d) {
-        var n = new Date(date);
-        n.setFullYear(date.getFullYear() + (y || 0));
-        n.setMonth(date.getMonth() + (m || 0));
-        n.setDate(date.getDate() + (d || 0));
-        return n;
+  // 1 or 0: LDL in last year, such that LDL <= 2.5?
+  function checkTarget() {
+    for( var i = 0, l = dTRMatches.length; i < l; i++ ){
+        if( dTRMatches[ i ].values()[ 0 ].scalar() <= tgtMax )
+          return true;
     }
+    return false;
+  }
 
-    // Checks for ldl labs performed within the last year
-    function hasLabCode() {
-        return resultList.match(targetLabCodes, start, end).length;
-    }
-
-    // Checks for diabetic patients
-    function hasProblemCode() {
-        return problemList.regex_match(targetProblemCodes).length;
-    }
-
-    // Checks if ldl meets parameters
-    function hasMatchingLabValue() {
-        for (var i = 0; i < resultList.length; i++) {
-            if (resultList[i].includesCodeFrom(targetLabCodes) && resultList[i].timeStamp() > start) {
-                if (resultList[i].values()[0].units() !== null &&
-                    resultList[i].values()[0].units().toLowerCase() === "mmol/L".toLowerCase()) {
-                    if (resultList[i].values()[0].scalar() <= ldlLimit) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    if (hasProblemCode()) {
-        emit("denominator", 1);
-        emit("denominator_" + patient['json']['primary_care_provider_id'], 1);
-        if(hasLabCode()) {
-            if(hasMatchingLabValue()) {
-                emit("numerator", 1);
-        emit("numerator_" + patient['json']['primary_care_provider_id'], 1);
-            }
-        }
-    }
-
-    // Empty Case
-    emit("numerator", 0);
-    emit("numerator_" + patient['json']['primary_care_provider_id'], 0);
-    emit("denominator", 0);
-    emit("denominator_" + patient['json']['primary_care_provider_id'], 0);
+  // Numerator must be a member of denominator and target groups
+  var inDen = checkDenominator(),
+      inNum = inDen && checkTarget();
+      emit( "denominator" + pid, inDen );
+      emit( "numerator"   + pid, inNum );
 }
