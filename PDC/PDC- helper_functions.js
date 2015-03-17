b66906a1fa2751bf38c2215ebb501edb64451e35
Derek Roberts <derek.roberts@gmail.com>
2015-03-17 16:34:34 -0700

    helper_functions case handling more flexible

--- PDC/PDC- helper_functions.js
+++ PDC/PDC- helper_functions.js
@@ -5,33 +5,51 @@
 
 
 /**
-* Filters a list of lab results:
-*   - lab, medication and condition codes (e.g. pCLOCD, whoATC, HC-DIN)
-*   - minimum and maximum values
-*   --> exclusive range, boundary cases are excluded
-*   - start and end dates
+* Filters a coded entry list:
+*   - parameters 1 & 2: list, codes
+*     - conditions(), immunizations(), medications(), results() or vitalSigns()
+*     - LOINC, pCLOCD, whoATC, SNOMED-CT, whoATC
+*   - parameters 3 - 6: dates or values, keep low/high pairs together
+*     - minimum and maximum values
+*     --> exclusive range, boundary cases are omitted
+*     - start and end dates
+*     --> inclusive range, boundary cases are counted
+*     - null/undefined/unsubmitted values are ignored
 */
-function filter_general( list, codes, p1, p2, p3, p4 ){
+function filter_general( list, codes, p3, p4, p5, p6 ){
   // Default variables = undefined
   var min, max, start, end, filteredList;
 
-  // Check parameters, which can be dates or scalars (numbers)
-  if( p1 instanceof Date ){
-    start = p1;
-    end   = p2;
-    min   = p3;
-    max   = p4;
-  }
-  else {
-    min   = p1;
-    max   = p2;
+  // Check parameters, which can be dates or number values (scalars)
+  if(( p3 instanceof Date )&&( p4 instanceof Date )){
     start = p3;
     end   = p4;
+    min   = p5;
+    max   = p6;
   }
+  else if(( p3 instanceof Date )&&( typeof p4 === 'number' )){
+    start = p3;
+    min   = p4;
+    max   = p5;
+  }
+  else if(( typeof p3 === 'number' )&&( typeof p4 === 'number' )){
+    min   = p3;
+    max   = p4;
+    start = p5;
+    end   = p6;
+  }
+  else if(( typeof p3 === 'number' )&&( p4 instanceof Date )){
+    min   = p3;
+    start = p4;
+    end   = p5;
+  }
+  else
+    emit( 'Parse error!', 1000000000 );
 
   // Use API's match functions to filter based on codes and dates
   //   - Immunizations, medications and results use an exact code match
   //   - Conditions use a regex match, so make sure to preface with '^'!
+  //   - undefined / null values are ignored
   if(( list[0] )&&( list[0].json._type === 'Condition' ))
     filteredList = list.regex_match( codes, start, end );
   else
@@ -71,7 +89,7 @@ function filter_activeMeds( matches ){
 
 /**
 * Used by filter_general() and filter_general()
-*   --> exclusive range, boundary cases are excluded
+*   - exclusive range, boundary cases are omitted
 */
 function filter_values( list, min, max ){
   // Default value
@@ -101,6 +119,7 @@ function isMatch( list ) {
 
 /**
 * T/F: Does the patient fall in this age range?
+*   - inclusive range, boundary cases are counted
 */
 function isAge( ageMin, ageMax ) {
   // Default values
