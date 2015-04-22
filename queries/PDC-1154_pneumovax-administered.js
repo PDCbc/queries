fa14ee9d7bdfcf2392b1207c5043dfa394a996ac
sdiemert <simon.diemert@gmail.com>
2015-04-22 22:02:51 +0000

    added pnemovax query file, currently empty

---
+++ queries/PDC-1154_pneumovax-administered.js
@@ -0,0 +1,11 @@
+/**
+ * Query Title: PDC-1154
+ * Query Type:  Ratio
+ * Desctiption: 
+ *	numerator: number of active patients (age >= 65) who have received pneumovax in the last 10 years 
+ *	denumerator: 
+ */
+ 
+function map( patient ){
+
+}
