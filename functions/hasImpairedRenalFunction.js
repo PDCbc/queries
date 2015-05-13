e20f876c465ab93ac7ac527edf9ac31efd7eaf62
Simon Diemert <simon.diemert@gmail.com>
2015-05-13 11:41:33 -0700

    added hasImpairedRenalFunction function and unit test

---
+++ functions/hasImpairedRenalFunction.js
@@ -0,0 +1,59 @@
+/*
+* Determines if the patient has impaired renal function.
+* 
+* Uses the definition of impaired renal function provided in the 
+*   Polarian Data Dictionary.
+* 
+* @param pt {object} -  a patient API object.
+* 
+* @return true if the patient has impaired renal function, false otherwise.
+*/
+function hasImpairedRenalFunction( pt ){
+    
+
+    if ( !pt || !pt.json ){
+
+        return false; 
+
+    }
+
+    var cons = pt.conditions(); 
+
+    if ( cons === undefined || cons === null || cons.length === 0 ){
+
+        return false; 
+
+    }
+
+
+    var codes = null; 
+
+    for ( var i = 0; i < cons.length; i++ ){
+
+        if(
+            cons[i].codes !== undefined && 
+            cons[i].codes !== null      && 
+            cons[i].codes.length !== 0  &&
+            cons[i].codes["ICD9"] !== undefined &&
+            cons[i].codes["ICD9"].length !== 0 
+
+        ){
+
+            var codes = cons[i].codes["ICD9"]; 
+
+            for ( var j=0; j < codes.length; j++ ){
+
+                if ( codes[j].match("^586.*") ){
+
+                    return true; 
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+}
---
+++ test/functions/test_hasImpairedRenalFunction.js
@@ -0,0 +1,173 @@
+function setUp(){
+    var obj = {
+        "_id": "1",
+        "emr_demographics_primary_key": "1",
+        "primary_care_provider_id": "cpsid",
+        "birthdate": 0,
+        "conditions" : [ 
+            { 
+                "_id": { "$oid": "551cce86c58406644d0000b1"},
+                "codes": { "ICD9": ["586"]}, 
+                "mood_code": "EVN",
+                "time": 1263167138, 
+                "description": "IMPAIRED RENAL FUNCTION"
+            } 
+        ]
+    }; 
+    return obj; 
+}
+
+module.exports = {
+
+    /*
+    * Test behavior when no patient is provided, i.e. undefined condition.
+    * 
+    * Expected: false.
+    */
+    testUndefinedPatient : function(){
+
+        var result = hasImpairedRenalFunction(); 
+
+        if (result === false ){
+
+            return {result : true, message:"test passed!"}
+
+        }else{
+
+            return {result:false, message:"expected false for input undefined"}
+
+        }
+    },
+
+    /*
+    * Test behavior when null patient is provided.
+    * 
+    * Expected: false.
+    */
+    testNullPatient : function(){
+
+        var result = hasImpairedRenalFunction(null); 
+
+        if (result === false ){
+
+            return {result : true, message:"test passed!"}
+
+        }else{
+
+            return {result:false, message:"expected false for input null"}
+
+        }
+
+    },
+
+    /*
+    * Test normal behavior with ICD9: 410
+    * 
+    * Expected: true.
+    */
+    testNormal : function(){
+
+        var c = setUp(); 
+
+        c = new hQuery.Patient(c);
+
+        var result = hasImpairedRenalFunction(c); 
+
+
+        if (result === true ){
+
+            return {result : true, message:"test passed!"}
+
+        }else{
+
+            return {result:false, message:"expected true for patient with ICD9: 586"}
+
+        }
+
+    }, 
+
+    /*
+    * Test behavior with empty conditions list
+    *
+    * Expected: false.
+    */
+    testEmptyConditionList : function(){
+
+        var c = setUp(); 
+
+        c.conditions = []; 
+
+        c = new hQuery.Patient(c);
+
+        var result = hasImpairedRenalFunction(c); 
+
+
+        if (result === false ){
+
+            return {result : true, message:"test passed!"}
+
+        }else{
+
+            return {result:false, message:"expected false for patient with empty conditions list"}
+
+        }
+
+    }, 
+
+    /*
+    * Test behavior with undefined conditions list
+    *
+    * Expected: false.
+    */
+    testUndefinedConditionList : function(){
+
+        var c = setUp(); 
+
+        delete c.conditions; 
+
+        c = new hQuery.Patient(c);
+
+        var result = hasImpairedRenalFunction(c); 
+
+
+        if (result === false ){
+
+            return {result : true, message:"test passed!"}
+
+        }else{
+
+            return {result:false, message:"expected false for patient with undefined conditions list"}
+
+        }
+
+    }, 
+
+    /*
+    * Test behavior with null conditions list
+    *
+    * Expected: false.
+    */
+    testNullConditionList : function(){
+
+        var c = setUp(); 
+
+        c.conditions = null; 
+
+        c = new hQuery.Patient(c);
+
+        var result = hasImpairedRenalFunction(c); 
+
+
+        if (result === false ){
+
+            return {result : true, message:"test passed!"}
+
+        }else{
+
+            return {result:false, message:"expected false for patient with null conditions list"}
+
+        }
+
+    } 
+
+}
