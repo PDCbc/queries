cc5416034145b181fc9a04a7214f63f3b4646d2f
fieranmason <fmason@nigelh-dell.cs.uvic.ca>
2015-07-13 12:01:53 -0700

    completed 934

---
+++ queries/PDC-934_COPDandPneumococcal.js
@@ -0,0 +1,22 @@
+//
+// PDC-934_COPDandPneumococcal
+//
+
+function map( patient ){
+  
+  var obj = patient.json;
+
+  var hv = hasPneumococcalVax( patient );
+
+  var hc = hasCOPD( patient );
+
+  var ia = activePatient( patient );
+
+  var numerator = hv && hc && ia;
+
+  var denominator = ia;
+
+  emit( "denominator_" + patient.json.primary_care_provider_id,  + denominator );
+
+  emit( "numerator_" + patient.json.primary_care_provider_id, + numerator   );
+}
