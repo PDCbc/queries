b650df5ce8d9bb47e192329b266cfbe9a89fa850
anitakat <ak.kanyange@gmail.com>
2015-07-13 14:12:18 -0700

    Changed hasEoLCondition to follow format of hasCKD etc.

--- functions/hasEoLCondition.js
+++ functions/hasEoLCondition.js
@@ -4,108 +4,98 @@
 * A patient is assumed to be Eol( or a physician would not be surprised if they passed in the next 6 months to a year)
 * if they have one or more of the SPICT clinical indicators listed below as ICD 9 codes.
 *
-* @param con {object} - a condition object from the patient api.
-* 
-* @return true if the condition falls within the definition of a cardiac event, 
-*   false otherwise. 
+* The exact SPICT indicators and the ICD9 codes/explanations can be found in the EoL Patient Definition doc
+*
+* @param pt - the patient api object
+ *
+ * @returns true if the patient has any of the conditions, false otherwise.
 */
 
-function hasEoLCondition( con ){
+function hasEoLCondition( pt ){
+    //check if the pt api is correctly set up.
+    if( pt === undefined || pt === null || pt.json === undefined || pt.json === null ){
 
-    //check for invalid input parameters
-    if (    con === undefined       || 
-            con === null            || 
-            con.json === undefined  || 
-            con.json === null 
-    ) {
-
-        return false; 
+        return false;
 
     }
 
+    var conditions = pt.json.conditions;
 
-    //check that the condition has a coded entry list 
-    if ( con.json.codes === undefined || con.json.codes === null ){
+    //check that we actually have conditions.
+    if( conditions === undefined || conditions === null || conditions.length === 0 ){
 
-        return false; 
-
-    }
-
-    if ( 
-        con.json.codes['ICD9'] === undefined  || 
-        con.json.codes['ICD9'] === null       || 
-        con.json.codes['ICD9'].length === 0
-    ){
-
-        return false; 
+        return false;
 
     }
 
-    var codes = con.json.codes['ICD9']; 
+    for( var c = 0; c < conditions.length; c ++ ){
 
-    // loop through the codes and match against the SPICT clinical indicators definition
-    // return true if we find a match.
-    for( var i = 0; i < codes.length; i ++ ){
+        //check to see that we have an ICD9 code system for the condition.
+        if( conditions[c] &&
+            conditions[c].codes &&
+            conditions[c].codes["ICD9"] &&
+            conditions[c].codes["ICD9"].length > 0
+        ){
 
-        if( typeof codes[i] === 'string' ){
+            //loop through the codes for this condition and check if any of them match CHF
+            for( var s = 0; s < conditions[c].codes["ICD9"].length; s++ ){
 
-            if ( // Neurological disease
-                codes[i].match("^784.5") ||
-                codes[i].match("^V41.6") ||
-                codes[i].match("^770.16")||
-                codes[i].match("^518.81")||
-                codes[i].match("^518.83")||
-                codes[i].match("^518.84")||
-                 // Liver disease
-                codes[i].match("^571.2")||
-                codes[i].match("^571.5")||
-                codes[i].match("^571.6")||
-                codes[i].match("^572.2")||
-                codes[i].match("^572.4")||
-                codes[i].match("^567.23")||
-                codes[i].match("^789.51")||
-                codes[i].match("^456.0")||
-                codes[i].match("^456.1")||
-                codes[i].match("^456.2")||
-                // Respiratory disease
-                codes[i].match("^491\..*")||
-                codes[i].match("^492\..*")||
-                codes[i].match("^494\..*")||
-                codes[i].match("^496\..*")||
-                codes[i].match("^V46.2")||
-                // Dementia/Frailty
-                codes[i].match("^780.31")||
-                codes[i].match("^780.32")||
-                // Kidney disease
-                codes[i].match("^585.4")||
-                codes[i].match("^585.5")||
-                // Heart/vascular disease
-                codes[i].match("^428\..*")||
-                codes[i].match("^402.01")||
-                codes[i].match("^402.11")||
-                codes[i].match("^402.91")||
-                codes[i].match("^404.01")||
-                codes[i].match("^404.03")||
-                codes[i].match("^404.11")||
-                codes[i].match("^404.13")||
-                codes[i].match("^404.91")||
-                codes[i].match("^404.93")||
-                // Cancer
-                codes[i].match("^196.9")||
-                codes[i].match("^197.7")||
-                codes[i].match("^198.3")||
-                codes[i].match("^198.5")
+                if(
+                    // Neurological disease
+                    conditions[c].codes["ICD9"][s].match("^784.5") ||
+                    conditions[c].codes["ICD9"][s].match("^V41.6") ||
+                    conditions[c].codes["ICD9"][s].match("^770.16")||
+                    conditions[c].codes["ICD9"][s].match("^518.81")||
+                    conditions[c].codes["ICD9"][s].match("^518.83")||
+                    conditions[c].codes["ICD9"][s].match("^518.84")||
+                    // Liver disease
+                    conditions[c].codes["ICD9"][s].match("^571.2")||
+                    conditions[c].codes["ICD9"][s].match("^571.5")||
+                    conditions[c].codes["ICD9"][s].match("^571.6")||
+                    conditions[c].codes["ICD9"][s].match("^572.2")||
+                    conditions[c].codes["ICD9"][s].match("^572.4")||
+                    conditions[c].codes["ICD9"][s].match("^567.23")||
+                    conditions[c].codes["ICD9"][s].match("^789.51")||
+                    conditions[c].codes["ICD9"][s].match("^456.0")||
+                    conditions[c].codes["ICD9"][s].match("^456.1")||
+                    conditions[c].codes["ICD9"][s].match("^456.2")||
+                    // Respiratory disease
+                    conditions[c].codes["ICD9"][s].match("^491\..*")||
+                    conditions[c].codes["ICD9"][s].match("^492\..*")||
+                    conditions[c].codes["ICD9"][s].match("^494\..*")||
+                    conditions[c].codes["ICD9"][s].match("^496\..*")||
+                    conditions[c].codes["ICD9"][s].match("^V46.2")||
+                    // Dementia/Frailty
+                    conditions[c].codes["ICD9"][s].match("^780.31")||
+                    conditions[c].codes["ICD9"][s].match("^780.32")||
+                    // Kidney disease
+                    conditions[c].codes["ICD9"][s].match("^585.4")||
+                    conditions[c].codes["ICD9"][s].match("^585.5")||
+                    // Heart/vascular disease
+                    conditions[c].codes["ICD9"][s].match("^428\..*")||
+                    conditions[c].codes["ICD9"][s].match("^402.01")||
+                    conditions[c].codes["ICD9"][s].match("^402.11")||
+                    conditions[c].codes["ICD9"][s].match("^402.91")||
+                    conditions[c].codes["ICD9"][s].match("^404.01")||
+                    conditions[c].codes["ICD9"][s].match("^404.03")||
+                    conditions[c].codes["ICD9"][s].match("^404.11")||
+                    conditions[c].codes["ICD9"][s].match("^404.13")||
+                    conditions[c].codes["ICD9"][s].match("^404.91")||
+                    conditions[c].codes["ICD9"][s].match("^404.93")||
+                    // Cancer
+                    conditions[c].codes["ICD9"][s].match("^196.9")||
+                    conditions[c].codes["ICD9"][s].match("^197.7")||
+                    conditions[c].codes["ICD9"][s].match("^198.3")||
+                    conditions[c].codes["ICD9"][s].match("^198.5")
 
-            ){
+                ){
 
-                return true; 
+                    return true;
 
+                }
             }
-
         }
-
     }
 
-    return false; 
-
+    return false;
 }
