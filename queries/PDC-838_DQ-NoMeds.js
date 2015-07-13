162392aa7667da82605f167345d5db9f2f99f7a0
fieranmason <fieranmason@gmail.com>
2015-07-13 15:58:56 -0700

    missed active patient constraint

--- queries/PDC-838_DQ-NoMeds.js
+++ queries/PDC-838_DQ-NoMeds.js
@@ -6,9 +6,11 @@ function map( patient ){
 
   var obj = patient.json;
 
-  var denominator = 1;
+  var nm = countMedications( patient ) === 0;
+  var ia = activePatient(patient);
 
-  var numerator = countMedications( patient ) === 0;
+  var denominator = ia;
+  var numerator = ia && nm;
 
   emit( "denominator_" + patient.json.primary_care_provider_id, denominator );
 
