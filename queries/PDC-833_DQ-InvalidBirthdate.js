961b0c62aea686aaf52304cb5d86aaa49f455a53
fieranmason <fmason@nigelh-dell.cs.uvic.ca>
2015-07-13 11:07:04 -0700

    completed 833

--- queries/PDC-833_DQ-InvalidBirthdate.js
+++ queries/PDC-833_DQ-InvalidBirthdate.js
@@ -1,25 +1,23 @@
 // Count active patients with an invalid date of birth,
 // excluding anyone over 120 years old
 //
-// DQ-DEM-03
+// PDC-833_DQ-InvalidBirthdate
 //
-// Note: E2E doesn't differentiate for active status
-// -- When that is added this query should be revised
 
 function map( patient ){
   // Patient is hQuery's definition of a person.  Use its JSON object.
   var obj = patient.json;
-  
+
   // Birthdate is a key in patient.json.  Select it.
   // Note: Formatted in milliseconds, so multiply by 1000.
   var bdNumber = obj.birthdate*1000;
-  
+
   // This is a date, so format it as one.  (we know this by context)
   var bd = new Date( bdNumber );
-  
+
   // Today's date.
   var now = new Date();
-    
+
   // Calculate age using hQuery.Patient's age method.
   var age = patient.age(now);
 
@@ -28,19 +26,21 @@ function map( patient ){
   function invalidDateFinder( d ){
     // Grab just the time;
     var time = d.getTime();
-    
+
     // Use the Number object's isNaN funtion.
     return Number.isNaN( time );
   }
-  
+
+  // Everyone is counted towards the denominator.
+  emit( "denominator_" + patient.json.primary_care_provider_id, 1 );
+
+  console.log('age: ' + age);
+
   // Numerator = 1 for fail, 0 for pass, since we're counting fails.
   if( invalidDateFinder( bd ) || age > 120 ){
-    emit( "Numerator", 1 );
+    emit( "numerator_" + patient.json.primary_care_provider_id, 1 );
   }
   else  {
-    emit( "Numerator", 0 );
+    emit( "numerator_" + patient.json.primary_care_provider_id, 0 );
   }
-  
-  // Everyone is counted towards the denominator.
-  emit( "Denominator", 1 );
 }
