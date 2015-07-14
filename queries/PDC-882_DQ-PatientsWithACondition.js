b3044bc80721eb0394da1eb6ff7e870140d3ae9e
fieranmason <fieranmason@gmail.com>
2015-07-14 09:54:09 -0700

    minor clean up

--- queries/PDC-882_DQ-PatientsWithACondition.js
+++ queries/PDC-882_DQ-PatientsWithACondition.js
@@ -7,8 +7,8 @@ function map( patient ){
   var obj = patient.json;
 
   var ia = activePatient(patient);
-  var denominator = ia ? countConditions( patient ) : 0;
-  var numerator = ia ? countConditions( patient ) > 0 : 0;
+  var denominator = ia ? countCodedConditions( patient ) : 0;
+  var numerator = ia ? countCodedConditions( patient ) > 0 : 0;
 
   emit( "denominator_" + patient.json.primary_care_provider_id, denominator );
 
