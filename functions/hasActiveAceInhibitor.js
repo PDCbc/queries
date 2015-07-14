d53317d08f88f3d0e419af7a90b5bed3e1d27dc4
fieranmason <fieranmason@gmail.com>
2015-07-14 16:19:02 -0700

    finished 1922

---
+++ functions/hasActiveAceInhibitor.js
@@ -0,0 +1,23 @@
+/*
+* Determines if a patient has an active statin.
+* - Uses the definition of a statin provided in isStatin().
+* - Uses the definition of a an active medication defined in isActiveMed
+*
+* @param pt {object} - the patient API object.
+*
+* @return - true if the patient has an active statin medication
+*               false otherwise.
+*/
+function hasActiveAceInhibitor( pt ){
+
+    var code = "^C09A*";
+
+    /*
+    var code1 = "^C10BAA";
+    var code2 = "^C10BX";
+    var codes3 = "^C10BA";
+    */
+
+    return hasActiveMed(pt, code, false);
+
+}
---
+++ queries/PDC-1922_AceInhibitor65+.js
@@ -0,0 +1,15 @@
+/**
+ * Query Title: PDC-1921_HNG-CoA65+
+ */
+function map( patient ){
+
+  var ap = activePatient(patient);
+  var ia = isAge(patient, 65);
+  var hm = hasActiveAceInhibitor(patient);
+
+  var denominator = ap && ia;
+  var numerator = ap && ia && hm;
+
+  emit( "denominator_"+patient.json.primary_care_provider_id, +denominator );
+  emit( "numerator_"+patient.json.primary_care_provider_id, +numerator );
+}
