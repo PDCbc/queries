f64e082146a11078d67e005b3af32d0aa2d4f5c7
fieranmason <fieranmason@gmail.com>
2015-07-15 09:25:39 -0700

    correcting c+p gremlins

--- functions/hasActiveAceInhibitor.js
+++ functions/hasActiveAceInhibitor.js
@@ -1,12 +1,7 @@
 /*
-* Determines if a patient has an active statin.
-* - Uses the definition of a statin provided in isStatin().
-* - Uses the definition of a an active medication defined in isActiveMed
-*
 * @param pt {object} - the patient API object.
 *
-* @return - true if the patient has an active statin medication
-*               false otherwise.
+* @return - true if the patient has an active medication
 */
 function hasActiveAceInhibitor( pt ){
 
