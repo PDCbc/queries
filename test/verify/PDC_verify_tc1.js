5c9155b2c61403aebed4aa7a68b9767924972405
Simon Diemert <simon.diemert@gmail.com>
2015-08-27 09:38:55 -0700

    fixed failing test for PDC-739

--- test/verify/PDC_verify_tc1.js
+++ test/verify/PDC_verify_tc1.js
@@ -1,56 +1,52 @@
 /**
-* A generic verifier function for PDC queries
-* @author: Simon Diemert
-* @date: 2015-05-07
-*/
+ * A generic verifier function for PDC queries
+ * @author: Simon Diemert
+ * @date: 2015-05-07
+ */
 
 /*
-* @param results - the results from running map reduce.
-*
-* @return an object, {result : boolean, message: String}
-*	the result field is true if the results are as expected, false otherwise.
-*	the message field contains a message which is displayed if the test fails
-*/
-function verify(results){
+ * @param results - the results from running map reduce.
+ *
+ * @return an object, {result : boolean, message: String}
+ *	the result field is true if the results are as expected, false otherwise.
+ *	the message field contains a message which is displayed if the test fails
+ */
+function verify(results) {
 
-	try{
+    try {
 
-		if(results.length !== 2){
+        if (results.length !== 2) {
 
-			return {result : false, message: "length of result was "+results.length+" not 2 as expected"};
+            return {result: false, message: "length of result was " + results.length + " not 2 as expected"};
 
-		}
+        }
 
-		if( results[0]["_id"] !== 'denominator_PROVIDER1' )
-		{
-			return {result : false, message: "id was " + results[0]["_id"] + " not denominator_PROVIDER1 as expected"};
-		}
-		else if( results[0].value !== 1 ){
+        if (results[0]["_id"] !== 'denominator_PROVIDER1') {
+            return {result: false, message: "id was " + results[0]["_id"] + " not denominator_PROVIDER1 as expected"};
+        }
+        else if (results[0].value !== 1) {
 
-			return {result : false, message: "denominator was " + results[0].value + " not 1 as expected"};
-		}
-		else if( results[1]["_id"] !== 'numerator_PROVIDER1' )
-		{
-			return {result : false, message: "id was " + results[1]["_id"] + " not numerator_PROVIDER1 as expected"};
-		}
-		else if( results[1].value !== 1 )
-		{
-			return {result : false, message: "numerator was " + results[1].value + " not 1 as expected"};
-		}
-		else
-		{
-			return {result : true, message: "test passed"};
-		}
+            return {result: false, message: "denominator was " + results[0].value + " not 1 as expected"};
+        }
+        else if (results[1]["_id"] !== 'numerator_PROVIDER1') {
+            return {result: false, message: "id was " + results[1]["_id"] + " not numerator_PROVIDER1 as expected"};
+        }
+        else if (results[1].value !== 1) {
+            return {result: false, message: "numerator was " + results[1].value + " not 1 as expected"};
+        }
+        else {
+            return {result: true, message: "test passed"};
+        }
 
-	}catch(e){
+    } catch (e) {
 
-		return {result : false, message: "failed to read result"};
+        return {result: false, message: "failed to read result"};
 
-	}
+    }
 
-	return {result : false, message: "test failed"};
+    return {result: false, message: "test failed"};
 }
 
 module.exports = {
-	verify: verify
+    verify: verify
 };
