7cb9a00eac1b110e067ff88ab08f05d62ca4f2f5
sdiemert <sdiemert@users.noreply.github.com>
2015-10-20 11:54:32 -0700

    removed comments from initiatives()

--- functions/initiatives.js
+++ functions/initiatives.js
@@ -2,22 +2,22 @@ function initatives(){
     return {
 
         "PPhRR"             : [
-            "cpsid",  //currently contains some test values.
+            "cpsid",
             "PROVIDER1",
             "PROVIDER2"
         ],
         "PracticeReflection": [
-            "cpsid",  //currently contains some test values.
+            "cpsid",
             "PROVIDER1",
             "PROVIDER2"
         ],
         "PopulationHealth"  : [
-            "cpsid",  //currently contains some test values.
+            "cpsid",
             "PROVIDER1",
             "PROVIDER2"
         ],
         "Attachment" : [
-            "cpsid",  //currently contains some test values.
+            "cpsid",.
             "PROVIDER1",
             "PROVIDER2"
         ]
