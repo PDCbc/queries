c1a74b0cd29d294b83d8d046325967d38136acd7
Fieran Mason <fieranmason@gmail.com>
2015-11-02 17:17:52 +0000

    bug fixes

--- queries/Retro-PDC-1738.js
+++ queries/Retro-PDC-1738.js
@@ -8,16 +8,19 @@ function map(patient) {
     if (filterProviders(patient.json.primary_care_provider_id, "ANY")) {
 
         
-        var pdcEpoch = new Date(2010, 0, 1);//first of january 2010
+        var pdcEpoch = new Date(2010, 0, 27);//27th of january 2010 -- dependent on day of month
 
         for(var referenceDate = new Date(pdcEpoch.getTime()); referenceDate.getTime()<Date.now(); referenceDate.setMonth(referenceDate.getMonth()+1))
         {
           var denominator = true;
           var numerator = denominator && activePatient(patient, referenceDate);
-          emit('{"denominator":"' + +denominator + '", ' + 
-                '"numerator":"' + +numerator + '", ' +
+          emit('{"value":"denominator", ' + 
                  '"pid":"' + patient.json.primary_care_provider_id + '", ' + 
-                 '"date":"' + referenceDate + '"}', 1);
+                 '"date":"' + referenceDate.getTime() + '"}', +denominator);
+          emit('{"value":"numerator", ' + 
+                 '"pid":"' + patient.json.primary_care_provider_id + '", ' + 
+                 '"date":"' + referenceDate.getTime() + '"}', +numerator);
+        
         }
     }
 
