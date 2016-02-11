4702b12fe0f07e3f1fe08f4143b2baeb26829e1e
Derek Roberts <derek.roberts@gmail.com>
2016-02-10 20:38:06 -0800

    Retro-PDC-1738 - Removed filter

--- queries/Retro-PDC-1738.js
+++ queries/Retro-PDC-1738.js
@@ -3,25 +3,20 @@
  * Query Type:  Ratio
  * Description: The ratio of active to inactive patients.
  */
-function map(patient) {
+function map( patient )
+{
+  var pdcEpoch = new Date(2010, 0, 27);//27th of january 2010 -- dependent on day of month
 
-    if (filterProviders(patient.json.primary_care_provider_id, "ANY")) {
+  for(var referenceDate = new Date(pdcEpoch.getTime()); referenceDate.getTime()<Date.now(); referenceDate.setMonth(referenceDate.getMonth()+1))
+  {
+    var denominator = true;
+    var numerator = denominator && activePatient( patient, referenceDate );
+    emit('{"value":"denominator", ' +
+           '"pid":"' + patient.json.primary_care_provider_id + '", ' +
+           '"date":"' + referenceDate.getTime() + '"}', +denominator);
+    emit('{"value":"numerator", ' +
+           '"pid":"' + patient.json.primary_care_provider_id + '", ' +
+           '"date":"' + referenceDate.getTime() + '"}', +numerator);
 
-        
-        var pdcEpoch = new Date(2010, 0, 27);//27th of january 2010 -- dependent on day of month
-
-        for(var referenceDate = new Date(pdcEpoch.getTime()); referenceDate.getTime()<Date.now(); referenceDate.setMonth(referenceDate.getMonth()+1))
-        {
-          var denominator = true;
-          var numerator = denominator && activePatient(patient, referenceDate);
-          emit('{"value":"denominator", ' + 
-                 '"pid":"' + patient.json.primary_care_provider_id + '", ' + 
-                 '"date":"' + referenceDate.getTime() + '"}', +denominator);
-          emit('{"value":"numerator", ' + 
-                 '"pid":"' + patient.json.primary_care_provider_id + '", ' + 
-                 '"date":"' + referenceDate.getTime() + '"}', +numerator);
-        
-        }
-    }
-
+  }
 }
